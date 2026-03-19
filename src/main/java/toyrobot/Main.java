03e50cfc4df23c780e6a4d2833f37feb416be397
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-03-19 18:50:59 +0000

    Add - JUnit to project

--- src/main/java/toyrobot/Main.java
+++ src/main/java/toyrobot/Main.java
@@ -1,12 +1,17 @@
-package org.danielmusselwhite;
+package toyrobot;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+    }
+
+    static int add(int a, int b)
+    {
+        return a+b;
     }
 }
--- src/test/java/toyrobot/MainTest.java
+++ src/test/java/toyrobot/MainTest.java
@@ -1,4 +1,12 @@
 package toyrobot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+    @Test
+    void sampleTest() {
+        assertEquals(5,Main.add(2,3));
+    }
 }
